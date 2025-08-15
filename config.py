3d68c67a58dc5766933a7de0590ad78e6656a539
xuxoperezpallas <46867823+xuxoperezpallas@users.noreply.github.com>
2025-08-15 20:39:53 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuración del servidor
+SERVER_IP = "localhost"  # Cambiar por IP pública si es necesario
+MANAGER_PORT = 5000
+
+# Configuración de Docker
+VNC_IMAGE = "dorowu/ubuntu-desktop-lxde-vnc"  # Imagen oficial
+RAM_POR_USUARIO = "1g"  # Memoria por contenedor
